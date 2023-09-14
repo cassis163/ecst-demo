27e32fd5ac454e8d733d06383a389d5cc2e96f7c
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2023-09-14 17:01:24 +0200

    Remove unused log

--- apps/article-service/backend/orm.go
+++ apps/article-service/backend/orm.go
@@ -3,14 +3,12 @@ package backend
 import (
 	"ecst-demo/cassis163/article-service/backend/models"
 
-	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitOrm() {
 	db := createDatabaseConnection()
-	log.Info("InitOrm")
 	db.AutoMigrate(&models.Article{})
 	article := models.Article{Name: "D42", Price: 100}
 	db.Create(&article)
